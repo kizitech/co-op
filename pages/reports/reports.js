ed5176f0989b14068753956144b487e787241418
kizitech <kizitoohani@gmail.com>
2023-11-10 15:11:54 +0100

    report day 2

--- pages/reports/reports.js
+++ pages/reports/reports.js
@@ -9,8 +9,24 @@ closeBtn.addEventListener("click", () => {
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
     } else {
-        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the iocns class
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
 }
+
+window.onload = function () {
+    Array.from(
+        document.getElementsByClassName("report-collapse-trigger")
+    ).forEach(function (item) {
+        item.addEventListener("click", function (e) {
+            var _btn = this;
+            console.log(_btn.parentNode.parentNode.classList.contains("open"));
+            if (_btn.parentNode.parentNode.classList.contains("open")) {
+                _btn.parentNode.parentNode.classList.remove("open");
+            } else {
+                _btn.parentNode.parentNode.classList.add("open");
+            }
+        });
+    });
+};
--- pages/savings/savings.js
+++ pages/savings/savings.js
@@ -41,8 +41,8 @@ closeBtn.addEventListener("click", () => {
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
     } else {
-        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the iocns class
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
 }
